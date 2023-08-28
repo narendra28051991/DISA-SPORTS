8afd9726fdaacc5ccb4096606841841c3327e010
narendra28051991 <narendra28051991@gmail.com>
2023-08-28 09:37:04 +0200

    changes made to background

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-light">
       <Title />
       <Navbar />
       <Introduction />
@@ -22,6 +22,6 @@ export default function Home() {
       <DisaCalendar />
       <Contact />
       <Updates />
-    </>
+    </div>
   )
 }
